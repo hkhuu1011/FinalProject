60f1e37d5d52761c9ce2ae6dacc04693e7d80e17
Han Khuu <h.khuu01@gmail.com>
2017-07-05 10:12:03 -0400

    added background img

--- Recipes/resources/views/welcome.blade.php
+++ Recipes/resources/views/welcome.blade.php
@@ -13,8 +13,13 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #3a1717;
+                background-image: url('../img/main_bg.jpg');
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-position: center;
+                background-size: 100% 100%;
+                color: #ffffff;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
